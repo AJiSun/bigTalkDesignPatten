ec822e8543307a94a4b1355484312dbcb9cc876f
AJiSun <sunj.guang@foxmail.com>
2017-07-09 15:48:26 +0800

    [add]简单工厂模式

---
+++ BigTalkDesignPattern/BigTalkDesignPattern/src/main/java/com/ajisun/simpleFactoryPattern/Operation.java
@@ -0,0 +1,12 @@
+package main.java.com.ajisun.simpleFactoryPattern;
+
+/**
+ * Copyright (c) 2017. Hand Enterprise Solution Company. All right reserved.
+ * Project Name:BigTalkDesignPattern
+ * Package Name:main.java.com.ajisun.simpleFactoryPattern
+ * Date:2017/7/9
+ * Create By:jiguang.sun@hand-china.com
+ */
+
+public class Operation {
+}
